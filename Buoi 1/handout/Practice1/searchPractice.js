e0be3b7843794e914f00dd533cb4b0c3e96b331c
BanhTuanAnh <anh98bnb@gmail.com>
2018-11-02 22:25:30 +0700

    hjb
    buổi 1

---
+++ Buoi 1/handout/Practice1/searchPractice.js
@@ -0,0 +1,11 @@
+'use strict'
+
+function search(input, target) {
+  for(var i=0;i<input.length;i++){
+      if(target==input[i]) return i;
+  }
+  return -1
+ // return  input.indexOf(target);  // Remove this line and change to your own algorithm
+}
+
+module.exports = search
